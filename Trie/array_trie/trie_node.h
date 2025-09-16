ae1f221762b45ae2c5eb9e12b5443bc779ed0986
Kirill <kirillkongrichanichenko@gmail.com>
2025-09-16 14:45:57 -0700

    fixing commit error

---
+++ Trie/array_trie/trie_node.h
@@ -0,0 +1,26 @@
+#ifndef TRI_NODE_H
+#define TRI_NODE_H
+
+class TrieNode
+{
+public:
+    TrieNode();
+
+    TrieNode* getChild(const int& index);
+    void setChild(const int& index, TrieNode* node);
+
+    bool getIsLeaf() const;
+    void setIsLeaf(bool val);
+
+private:
+
+    // pointer array for child nodes of each node
+    TrieNode* children[26];
+
+    // Used for indicating ending of string
+    bool isLeaf;
+};
+
+
+
+#endif //TRI_NODE_H
